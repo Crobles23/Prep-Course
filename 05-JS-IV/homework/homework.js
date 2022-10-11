cb6b13a2ff7272f448e9e3ebd57c96d8495352bd
Crobles23 <CristianRobles_1998@outlook.com>
2022-10-11 00:08:16 -0500

    Todo sigue avanzando, yo tambien

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -142,8 +142,8 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-  producto.calcularPrecioDescuento = function(){let descuento = this.precio - this.precio * this.porcentajeDeDescuento; return descuento;}
-  return producto;;
+  producto.calcularPrecioDescuento = function (){let descuento = this.precio - this.precio * this.porcentajeDeDescuento; return descuento;}
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
